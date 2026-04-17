efeb3b0c31b7351a2f384ed8e9d89893610c007b
Diego Righi <diego_righi@icloud.com>
2026-04-17 09:30:00 +0100

    initial project setup with spring boot 4
    generated from start.spring.io with web, jpa, security, actuator, liquibase

---
+++ src/main/java/com/challange/sky/api/SkyCodechallangeApplication.java
@@ -0,0 +1,13 @@
+package com.challange.sky.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyCodechallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyCodechallangeApplication.class, args);
+    }
+
+}
